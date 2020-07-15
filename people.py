0f39a5a025842a1b9f2cf00febe47cc32d7b7ea3
Saheed Lamina git config --global user.email you@example.comgit config --global user.name Saheed <agbolamina@hotmail.com>
2020-07-15 15:13:05 +0100

    Updated files with plane assignments

--- people.py
+++ people.py
@@ -17,34 +17,15 @@ class Staff(People):
     def s_details(self):
         print(self.f_name + self.l_name + self.age + self.gender + self.id_number + self.passport_number)
 
-# this was an experiment with passwords and how they work
-class Managers(Staff):
-    def changes(self):
-        username = input("Please enter your username: ")
-        password = input("Please enter your password: ")
-
-
-
-
-
-        if username == "manager" and password == "pass1":
-            options = input(f"\nWelcome {self.f_name.title()}, which details would you like to amend? ")
-            if options == "amend plane details":
-                pass
-        elif username != "manager" and password != "pass1":
-            print("Please check that data is correct")
-        # elif options == "assign a plane to a route":
-
-
-
-flight =
 
 
 
 
 
 
-        # planes = input("plane1, plane2, plane3 ")
+
+
+
 
 
 
@@ -52,5 +33,4 @@ flight =
 # agbo.details()
 # bari = Staff("bari ", "allali ", "24 ", "male ", "213 ", "fr213")
 # bari.s_details()
-bob = Managers("bob ", "smith ", " 45 ", "female ", "123 ", "12345 ")
-bob.changes()
+

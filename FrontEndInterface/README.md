698bea1609e47195da7b002787657d2e660bc90b
gia-bartlett <g.bartlett89@gmail.com>
2020-07-15 15:13:09 +0100

    updated readme

---
+++ FrontEndInterface/README.md
@@ -0,0 +1,34 @@
+## FRONT END INTERFACE README  
+
+#### Menu:  
+Created a menu that imports the database functions.  
+Options:  
+* Create a booking (using the add passenger functions)  
+* Amend a booking (using the delete passenger functions)  
+* View an existing booking (using the passenger table)  
+
+```python
+class Menu_interface():
+    def create_booking(self):
+        obj1 = Adding_passenger_info()
+        obj1.storing_passenger_info()
+```
+
+#### Welcome:  
+Created a welcome screen hosting a series of options for the Airport Assistant to choose from.  
+Options:  
+* Create a new booking  
+* View an existing booking  
+* Amend an existing booking  
+* View T&Cs  
+* Count down to booking  
+* Help  
+
+This welcome screen is fed by the Menu Interface
+
+```python
+def user_interaction_passengers(self):
+
+    print("\n*** Airport Assistant Passenger Booking Interface ***")
+    assistant_first_name = str(input("Please enter your name: \n")).title()
+```
--- aircraft.py
+++ aircraft.py
@@ -11,6 +11,8 @@ class Long_Haul(Aircraft):  # create a long_haul for flights over 3000km
         self.num_rows = num_rows  # number of rows for passengers
         self.num_seats_per_row = num_seats_per_row  # number of seats across each row
 
+
+
     def seating_plan(self):  # returns allowed rows and seats as a tuple
         return (range(1, self.num_rows + 1), "ABCDEFGHJK" [:self.num_seats_per_row])
 # this produces a range object which is used as an iterable series of row numbers. The slice method returns
@@ -30,9 +32,16 @@ class Short_Haul(Aircraft):  # create a short_haul for flights under 2999km
 
 
 
+# pa747 = Long_Haul("Boeing", "PythonAir", 263, 25, 10)  # creating an instance of a long haul with 250 passengers and 13 crew
+# aa220 = Short_Haul("Airbus", "AgileAir", 113, 18, 6)  # creating an instance of a short haul with 108 passengers and 5 crew
+# print(pa747.seating_plan())  # (range(1, 26), 'ABCDEFGHJK') (half open range)
+# print(aa220.seating_plan())  # (range(1, 19), 'ABCDEF')
 
 # class Helicopter(Aircraft):
 #     def __init__(self, model, airline, capacity, num_seats):
 #         super().__init__(model, airline)
 #         self.capacity = capacity
 #         self.num_seats = num_seats
+
+
+passengers = ["Elisabeth Allen", "Ellen Barber", "Helen Bishop", "John Brady", "Alexander Cairns", "Lucile Carter", "Norman Chambers", "Edward Colley", "Ruth Dodge", "Olive Earnshaw"]
